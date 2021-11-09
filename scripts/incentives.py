bd7135a18ab532b13a42da75a7328d5b0354252b
aaron.barlow <aaron.barlow@nps.edu>
2021-11-08 21:12:20 -0800

    WethInterface.sol included

--- scripts/incentives.py
+++ scripts/incentives.py
@@ -90,6 +90,7 @@ def main():
             attempt2 = 0
             unwrap = False
             while attempt2 <= 10 and unwrap != True:
+                print('ABOUT TO ATTEMPT UNWRAP')
                 try:
                     tx2 = unwrap_all(account)
                     tx2.wait(1)
